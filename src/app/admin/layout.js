c05ae2d0d65831941d9295a58377ad121793d5a1
xyzzy <xyzzy@ragbaz.cloud>
2026-03-29 13:18:17 +0000

    admin-ui: split admin styles into route-scoped admin.css

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,19 +1,12 @@
-import { JetBrains_Mono } from "next/font/google";
+import "./admin.css";
 import AdminHeader from "@/components/admin/AdminHeader";
 import site from "@/lib/site";
 import ToastHost from "@/components/common/ToastHost";
 import AdminThemeWrapper from "@/components/admin/AdminThemeWrapper";
 
-const jetbrainsMono = JetBrains_Mono({
-  variable: "--font-admin",
-  subsets: ["latin"],
-  display: "swap",
-  weight: ["300", "400", "500", "600", "700"],
-});
-
 export default function AdminLayout({ children }) {
   return (
-    <AdminThemeWrapper fontVariable={jetbrainsMono.variable}>
+    <AdminThemeWrapper>
       <AdminHeader logoUrl={site.logoUrl} />
       <ToastHost />
       {children}
